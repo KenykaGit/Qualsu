9a60427140182283e1ff117584ff9f876ffce7f9
Kenyka <maratkanovartem89@mail.ru>
2024-09-27 22:50:16 +0300

    create site

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const textElement = document.querySelector('.text');
+const logoElement = document.querySelector('.logo');
+
+const link1Element = document.querySelector('.link-1');
+const link2Element = document.querySelector('.link-2');
+const link3Element = document.querySelector('.link-3');
+
+logoElement.style.animation = 'logo 1.3s ease-in-out forwards';
+
+logoElement.addEventListener('animationend', () => {
+  textElement.style.animation = 'text 3s ease forwards';
+
+    textElement.addEventListener('animationend', () => {
+        link1Element.style.opacity = 1;
+        link1Element.style.animation = 'link 1.5s ease forwards';
+
+        link1Element.addEventListener('animationend', () => {
+            link2Element.style.opacity = 1;
+            link2Element.style.animation = 'link 1.5s ease forwards';
+
+            link2Element.addEventListener('animationend', () => {
+                link3Element.style.opacity = 1;
+                link3Element.style.animation = 'link 1.5s ease forwards';
+
+                link3Element.addEventListener('animationend', () => {
+                    link1Element.style.animation = '';
+                    link2Element.style.animation = '';
+                    link3Element.style.animation = '';
+                    
+                });
+            });
+        });
+    });
+});
--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -3,7 +3,14 @@ tailwind.config = {
       extend: {
         colors: {
           clifford: '#da373d',
-        }
+          main: {
+            Q: "#C178FF",
+            S: "#8365FF",
+            U: "#76A4FF",
+            purple: "#1D121B",
+            whitepurple: "#2D1229"
+          },
+        },
       }
     }
   }
